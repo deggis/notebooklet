9939a2eff536015bcddd975dad477459245486b4
Heikki Salo <heikki.ao.salo@iki.fi>
2010-10-01 08:05:37 +0300

    Implemented #tag parsing from notes at note saving.

--- evently/profile/profileReady/selectors/form/submit.js
+++ evently/profile/profileReady/selectors/form/submit.js
@@ -2,8 +2,10 @@ function() {
   var form = $(this);
   var fdoc = new Object();
   fdoc.created_at = new Date();
-  fdoc.note = $("#note").val();
+  note = $("#note").val();
+  fdoc.note = note;
   fdoc.env = $("#env").val();
+  fdoc.tags = note.match(/\#[a-z]+/g);
   $$(this).app.db.saveDoc(fdoc, {
     success : function() {
       form[0].reset();
