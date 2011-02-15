7aeda2aa9195548becf91970feed085a3b2a55f6
Heikki Salo <heikki.ao.salo@iki.fi>
2011-02-15 19:50:28 +0200

    Added temporary bash script to start CouchDB with bidirectional replication.

---
+++ misc/couchdb_with_replications.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ssh -v -NL5985:localhost:5984 bebop &
+./couchdb &
+sleep 3
+curl -H "Content-Type: application/json" -X POST http://127.0.0.1:5984/_replicate -d '{"source":"http://127.0.0.1:5985/notebooklet", "target":"notebooklet"}'
+sleep 1
+curl -H "Content-Type: application/json" -X POST http://127.0.0.1:5984/_replicate -d '{"source":"notebooklet", "target":"http://127.0.0.1:5985/notebooklet"}'
+
