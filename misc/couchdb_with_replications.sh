16d23d183476094bf972d656706805cab6559c64
Heikki Salo <heikki.ao.salo@iki.fi>
2011-02-15 20:06:15 +0200

    Set replication to be continuous in temp script.

--- misc/couchdb_with_replications.sh
+++ misc/couchdb_with_replications.sh
@@ -3,7 +3,7 @@
 ssh -v -NL5985:localhost:5984 bebop &
 ./couchdb &
 sleep 3
-curl -H "Content-Type: application/json" -X POST http://127.0.0.1:5984/_replicate -d '{"source":"http://127.0.0.1:5985/notebooklet", "target":"notebooklet"}'
+curl -H "Content-Type: application/json" -X POST http://127.0.0.1:5984/_replicate -d '{"source":"http://127.0.0.1:5985/notebooklet", "target":"notebooklet", "continuous":true}'
 sleep 1
-curl -H "Content-Type: application/json" -X POST http://127.0.0.1:5984/_replicate -d '{"source":"notebooklet", "target":"http://127.0.0.1:5985/notebooklet"}'
+curl -H "Content-Type: application/json" -X POST http://127.0.0.1:5984/_replicate -d '{"source":"notebooklet", "target":"http://127.0.0.1:5985/notebooklet","continuous":true}'
 
